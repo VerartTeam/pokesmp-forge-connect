7dedf5d9b0756a45116ca5af218fa236c9807e0c
Rishon Jaffe <rishonjaffe9@gmail.com>
2024-01-03 12:06:20 +0200

    remove unnemove cessary תy files

--- src/main/java/dev/rishon/pokesmpforgeconnect/mixin/MultiplayerScreenMixin.java
+++ src/main/java/dev/rishon/pokesmpforgeconnect/mixin/MultiplayerScreenMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(JoinMultiplayerScreen.class)
 public class MultiplayerScreenMixin {
 
-    @Inject(at = @At("TAIL"), method = "init")
+    @Inject(at = @At("TAIL"), method = "<init>")
     private void init(CallbackInfo info) {
         PokeSMPConnect.logger.info("Multiplayer Callback");
         PokeSMPConnect.connectToPokeSMP();
